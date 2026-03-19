b49b57f961312dc936fd30e938b99e3d865959f7
ganeshmoorthy20040905 <ganeshmoorthy20040905@gmail.com>
2026-03-19 15:45:27 +0530

    feat: Implement Axios API client with token-based authentication and request interceptor.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,4 +5,17 @@ const API = axios.create({
     withCredentials: true,
 });
 
+let accessToken = null;
+
+export const setAccessToken = (token) => {
+    accessToken = token;
+};
+
+API.interceptors.request.use((config) => {
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+});
+
 export default API;
